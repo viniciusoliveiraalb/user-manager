8e3d8124d5f15ba7a0bf3d7d9adc79da283ee503
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2024-06-16 19:31:46 -0400

    subindo o projeto para meu repositório.

---
+++ database/cliente.py
@@ -0,0 +1,7 @@
+CLIENTES = [
+    {"id":1, "nome": "Vinicius1", "email": "teste@teste1.com"},
+    {"id":2, "nome": "Vinicius2", "email": "testwee@teste2.com"},
+    {"id":3, "nome": "Vinicius3", "email": "testrwere@teste3.com"},
+    {"id":4, "nome": "Vinicius4", "email": "testerere@teste4.com"},
+    {"id":5, "nome": "Vinicius5", "email": "tesrete@teste5.com"},
+]
